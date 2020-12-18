03519ea436eef432576ba5b83c8f7b9985d23871
Ian Warshak <iwarshak@stripey.net>
2020-12-18 11:45:40 -0600

    initial

---
+++ test/foo_web/live/page_live_test.exs
@@ -0,0 +1,11 @@
+defmodule FooWeb.PageLiveTest do
+  use FooWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  test "disconnected and connected render", %{conn: conn} do
+    {:ok, page_live, disconnected_html} = live(conn, "/")
+    assert disconnected_html =~ "Welcome to Phoenix!"
+    assert render(page_live) =~ "Welcome to Phoenix!"
+  end
+end
